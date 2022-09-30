5f799659899c7f54b5f7288a55cd662ccc5c0c33
20201tiimi0136 <lavinianina@outlook.com>
2022-09-30 12:00:37 -0300

    Inicio do projeto

---
+++ app/src/main/java/mariano/lavinia/galeriapublica/ImageDataComparator.java
@@ -0,0 +1,19 @@
+package mariano.lavinia.galeriapublica;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+
+class ImageDataComparator extends DiffUtil.ItemCallback<ImageData> {
+    @Override
+    public boolean areItemsTheSame(@NonNull ImageData oldItem,
+                                   @NonNull ImageData newItem) {
+        // Id is unique.
+        return oldItem.uri.equals(newItem.uri);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull ImageData oldItem,
+                                      @NonNull ImageData newItem) {
+        return oldItem.uri.equals(newItem.uri);
+    }
+}
